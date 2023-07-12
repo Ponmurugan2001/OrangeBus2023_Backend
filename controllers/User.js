584d788011ea292a604a7c3d6d270f690f38e164
Ponmurugan2001 <20d228@psgtech.ac.in>
2023-07-12 23:55:21 +0530

    login/register_endpoint complited

--- controllers/User.js
+++ controllers/User.js
@@ -1,6 +1,7 @@
 const User=require("../models/User")
 
 const bcrypt=require('bcryptjs')
+const jwt = require("jsonwebtoken")
 
 
 exports.createUser=async(req,res)=>{
@@ -9,7 +10,7 @@ exports.createUser=async(req,res)=>{
         if(existingUser){
             return res.status(200) .send({message:"user with email alrready exist",success:false})
         }
-        const hashedPassword=await bcrypt.hash(req.body.password,11);
+        const hashedPassword=await bcrypt.hash(req.body.password,6);
         req.body.password=hashedPassword;
         const newUser=new User(req.body)
         await newUser.save();
@@ -17,7 +18,36 @@ exports.createUser=async(req,res)=>{
         
     } catch (error) {
         console.log(error)
-        return res.status(401) .send({message:"something went wrong on db",success:false})
+        return res.status(200) .send({message:"something went wrong on db",success:false})
     }
     
+}
+exports.loginUser=async(req,res)=>{
+    
+    try {
+        console.log("insidet try" );
+        const userexist= await User.findOne({emailID:req.body.emailID});
+       
+        if(!userexist){
+            
+            return res.status(200) .send({message:"user does not exist",success:false})
+
+        }
+        
+        const passwordmatch = await bcrypt.compare(req.body.password, userexist.password );
+        
+        if(!passwordmatch){return res.status(200) .send({message:"incorrect password ",success:false})}
+        const token =jwt.sign({userid:userexist._id},process.env.secret_token,{expiresIn:"1d"});
+
+        return res.status(201) .send({message:"login succesfull",success:true,data: token })
+
+        
+
+
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(401) .send({message:"something went wrong on db",success:false})
+        
+    }
 }
--- routes/User.js
+++ routes/User.js
@@ -1,11 +1,13 @@
 const router=require('express').Router()
 
-const {createUser}=require("../controllers/User")
+const {createUser,loginUser}=require("../controllers/User")
+
 
 
 
 http://localhost:9000/api/user/register
 router.post('/register',createUser);
+router.post('/login',loginUser);
 
 
 module.exports=router;
